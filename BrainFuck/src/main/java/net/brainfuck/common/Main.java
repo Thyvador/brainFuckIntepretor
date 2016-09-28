e22c81ea495ad9ce640236ed15a66fcabf154229
Fitzzzz <david.lang697@gmail.com>
2016-09-28 10:36:19 +0200

    Changement main pour lancer l'intrepreteur

--- BrainFuck/src/main/java/net/brainfuck/common/Main.java
+++ BrainFuck/src/main/java/net/brainfuck/common/Main.java
@@ -2,6 +2,8 @@ package net.brainfuck.common;
 
 import net.brainfuck.common.exception.FileNotFoundException;
 
+import java.io.IOException;
+
 public class Main {
 	
 	private Main() throws FileNotFoundException, java.io.FileNotFoundException {
@@ -9,6 +11,15 @@ public class Main {
 		Memory m = Memory.getInstance();
 		Reader r = new FileReader("C:\\Users\\user\\Desktop\\indianwomen.txt");
 		Interpretor i = new Interpretor(m,r);
+
+		try {
+			i.interprate();
+		} catch (net.brainfuck.common.exception.IOException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		System.exit(0);
 	}
 	
