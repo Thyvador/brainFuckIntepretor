c52f64ed842770c6f3f70cb16753fc5befed2168
Fitzzzz <david.lang697@gmail.com>
2016-09-28 14:06:14 +0200

    Changement main pour lancer l'intrepreteur

--- BrainFuck/src/main/java/net/brainfuck/common/Interpretor/RightExecute.java
+++ BrainFuck/src/main/java/net/brainfuck/common/Interpretor/RightExecute.java
@@ -5,7 +5,7 @@ import net.brainfuck.common.Memory;
 /**
  * Created by davidLANG on 28/09/2016.
  */ // Concret class for right
-class RightExecute implements InterpretorInterface {
+public class RightExecute implements InterpretorInterface {
     @Override
     public void execute(Memory machine) {
         machine.right();
