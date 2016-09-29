b67a4a08a393dbd488429e6c7c61607d751c450a
arsebac <mf403165@etu.unice.fr>
2016-09-29 22:14:36 +0200

    Added some file which throw errors

---
+++ BrainFuck/src/test/java/net/brainfucktest/common/TestErrors.java
@@ -0,0 +1,18 @@
+package net.brainfucktest.common;
+
+import net.brainfuck.common.Main;
+import net.brainfuck.exception.FileNotFoundException;
+
+/**
+ * Created by François Melkonian
+ * Some test about errors
+ */
+public class TestErrors {
+    public static void main(String[] args) throws FileNotFoundException, java.io.FileNotFoundException {
+        //new Main("BrainFuck\\src\\test\\resources\\assets\\brainfucktest\\common\\OutOfBoundLeft.bf");
+        //new Main("BrainFuck\\src\\test\\resources\\assets\\brainfucktest\\common\\OutOfBoundRight.bf");
+        // new Main("BrainFuck\\src\\test\\resources\\assets\\brainfucktest\\common\\OverflowDecr.bf");
+         new Main("BrainFuck\\src\\test\\resources\\assets\\brainfucktest\\common\\OverflowIncr.bf");
+    }
+
+}
