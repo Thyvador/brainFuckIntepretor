860f94cfe2ec6ef7f88a112bf1cfc76eeabc55b2
arsebac <mf403165@etu.unice.fr>
2016-10-06 16:57:17 +0200

    Change directory

--- BrainFuck/src/test/java/net/brainfucktest/common/TestErrors.java
+++ BrainFuck/src/test/java/net/brainfucktest/common/TestErrors.java
@@ -5,14 +5,15 @@ import net.brainfuck.exception.FileNotFoundException;
 
 /**
  * Created by François Melkonian
+ * pour certain le chemin est Brainfuck/src/test/resources/assets/brainfucktest/common/
  * Some test about errors
  */
 public class TestErrors {
     public static void main(String[] args) throws FileNotFoundException, java.io.FileNotFoundException {
-        new Main("/assets/brainfucktest/common/OutOfBoundLeft.bf");
-        //new Main("/assets/brainfucktest/common/OutOfBoundRight.bf");
-        // new Main("/assets/brainfucktest/common/OverflowDecr.bf");
-        // new Main("/assets/brainfucktest/common/OverflowIncr.bf");
+        new Main("Brainfuck/src/test/resources/assets/brainfucktest/common/OutOfBoundLeft.bf");
+        //new Main("Brainfuck/src/test/resources/assets/brainfucktest/common/OutOfBoundRight.bf");
+        // new Main("Brainfuck/src/test/resources/assets/brainfucktest/common/OverflowDecr.bf");
+        // new Main("Brainfuck/src/test/resources/assets/brainfucktest/common/OverflowIncr.bf");
     }
 
 }
