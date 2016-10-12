2e8aa965cea6df85bce2ac904298a414f4a57c0e
Fitzzzz <david.lang697@gmail.com>
2016-10-12 09:02:24 +0200

    Changement main pour lancer l'intrepreteur

--- BrainFuck/src/main/java/net/brainfuck/Main.java
+++ BrainFuck/src/main/java/net/brainfuck/Main.java
@@ -4,19 +4,23 @@ import net.brainfuck.common.BfReader;
 import net.brainfuck.common.Memory;
 import net.brainfuck.common.Reader;
 import net.brainfuck.exception.*;
+import net.brainfuck.exception.Exception;
 import net.brainfuck.exception.FileNotFoundException;
 import net.brainfuck.exception.IOException;
 import net.brainfuck.interpreter.Interpreter;
 
+import static net.brainfuck.ArgumentConstante.PATH;
+
 
 public class Main {
 
-	public Main(String filename){
+	public Main(String[] args) throws FileNotFoundException{
 
 		try {
+			ArgumentAnalizer a = new ArgumentAnalizer(args);
 			Memory m = new Memory();
-			Reader r = new BfReader(filename);
-			Interpreter i = new Interpreter(m,r);
+			Reader r = new BfReader(a.getArgument(PATH));
+			Interpreter i = new Interpreter(m,r, a.getFlags());
 			i.interprate();
 			System.out.println(m);
 		} catch (IOException | SyntaxErrorException | FileNotFoundException e) {
@@ -26,22 +30,25 @@ public class Main {
 			System.exit(1);
 		} catch (MemoryOverFlowException e){
 			System.exit(2);
+		} catch (Exception e) {
+			// Ajouter par argumentAnalizer il faut une nouvelle exception pour les arguments
+			e.printStackTrace();
 		}
 		System.exit(0);
 	}
 
-	private Main(){
-		new Main("/assets/brainfuck/common/OutOfBoundLeft.bf"); // On lance sur un fichier au hasard
+	private Main() throws FileNotFoundException {
+//		String[] args = {"-p", "/assets/brainfuck/common/OutOfBoundLeft.bf"};
+//		new Main(args); // On lance sur un fichier au hasard
 	}
 
 	// prepare the executable jar
-	public static void main(String[] args){
-        if(args.length == 2 && args[1].equals(("-p"))){
-            new Main(args[1]);
-        }else if(args.length == 0) {
-			System.out.println("Usage : - p [FILE]");
-			System.exit(0);
-        }
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("Working Directory = " +
+				System.getProperty("user.dir"));
+		String[] args2 = {"-p", "Brainfuck/src/main/resources/assets/brainfuck/common/res.bf", "--rewrite"};
+		new Main(args2); // On lance sur un fichier au hasard
+		//new Main(args);
 	}
 
 }
