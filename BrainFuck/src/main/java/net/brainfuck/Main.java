835cf1c6fe8767b3161cbe72e003750e6e28fd46
Fitzzzz <david.lang697@gmail.com>
2016-10-20 14:02:59 +0200

    Ajout de commentaire

--- BrainFuck/src/main/java/net/brainfuck/Main.java
+++ BrainFuck/src/main/java/net/brainfuck/Main.java
@@ -2,37 +2,33 @@ package net.brainfuck;
 
 import static net.brainfuck.common.ArgumentConstante.PATH;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.RuntimeMXBean;
-import java.util.Arrays;
-import java.util.List;
 import net.brainfuck.common.ArgumentAnalyzer;
 import net.brainfuck.common.BfReader;
 import net.brainfuck.common.Memory;
 import net.brainfuck.common.Reader;
 import net.brainfuck.exception.*;
-import net.brainfuck.exception.Exception;
-import net.brainfuck.exception.FileNotFoundException;
-import net.brainfuck.exception.IOException;
 import net.brainfuck.interpreter.Interpreter;
 
 
 public class Main {
 
-	public Main(String[] args){
-		try {
-			/*for (String z:arguments) {
-				System.out.println(z);
-			}*/
-			ArgumentAnalyzer a = new ArgumentAnalyzer(args);
-			Memory m = new Memory();
-			for (String z :
-					args) {
-				System.out.println(z);
-			}
-			System.out.println("get arg : "+args);
-			Reader r = new BfReader(a.getArgument(PATH));
+	private void printUsage() {
+		System.out.println("Usage : bfck.sh -p FILE [--rewrite]");
+	}
 
+	public Main(String[] args){
+		if (args.length == 0) {
+			this.printUsage();
+			System.exit(0);
+		}
+		try {
+			ArgumentAnalyzer a = new ArgumentAnalyzer(args);
+			if (a.getArgument(PATH) == null) {
+				this.printUsage();
+				System.exit(0);
+			}
+			Memory m = new Memory();
+			Reader r = new BfReader(a.getArgument(PATH));
 			Interpreter i = new Interpreter(m,r, a);
 			i.interprate();
 			System.out.println(m);
@@ -48,25 +44,8 @@ public class Main {
 	}
 
 
-	private Main() {
-//		String[] args = {"-p", "/assets/brainfuck/common/OutOfBoundLeft.bf"};
-//		new Main(args); // On lance sur un fichier au hasard
-	}
-
-	static RuntimeMXBean runtimeMxBean;
-	static List<String> arguments;
-	// prepare the executable jar
 	public static void main(String[] args) {
-		runtimeMxBean = ManagementFactory.getRuntimeMXBean();
-		arguments = runtimeMxBean.getInputArguments();
-		/*String[] s = new String[arguments.size()];
-		for (int i = 0; i < arguments.size(); i++) {
-			s[i]=arguments.get(i);
-		}*/
-		System.out.println(Arrays.toString(args));
-		System.out.println(arguments);
-		new Main(args); // On lance sur un fichier au hasard
-		//new Main(args);
+		new Main(args);
 	}
 
 }
