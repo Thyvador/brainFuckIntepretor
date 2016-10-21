a998ecefebdc04a574b151f39137fd8f922de5f2
arsebac <mf403165@etu.unice.fr>
2016-10-21 13:29:32 +0200

    dqsmldqsd

--- BrainFuck/src/test/java/net/brainfucktest/common/Test.java
+++ BrainFuck/src/test/java/net/brainfucktest/common/Test.java
@@ -5,7 +5,7 @@ import net.brainfuck.Main;
 public class Test {
 
     public static void main(String[] args) {
-
+//TE
             String[] args2 = {"-p", "Brainfuck/src/test/resources/assets/brainfucktest/common/Example.bf"};
         new Main(args2); // On lance sur un fichier au hasard
       //  new Main("Brainfuck/src/test/resources/assets/brainfucktest/common/syntax/allInOneLine.bf");
