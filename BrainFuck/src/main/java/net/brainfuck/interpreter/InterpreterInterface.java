3b782870258cf1288ab55cf3a4227348c7ef0c53
Fitzzzz <david.lang697@gmail.com>
2016-11-02 15:09:20 +0100

    Ajout de translate() et rewrite() dans InterpreterInterface

--- BrainFuck/src/main/java/net/brainfuck/interpreter/InterpreterInterface.java
+++ BrainFuck/src/main/java/net/brainfuck/interpreter/InterpreterInterface.java
@@ -24,9 +24,19 @@ public interface InterpreterInterface {
      * @throws MemoryOutOfBoundsException throw by memory
      * @throws MemoryOverFlowException  throw by memory
      * @throws IOException throw by memory
-     * @throws BracketsParseException 
+     * @throws BracketsParseException throw by JumpExecute and BackExecute
      */
     void execute(Memory memory, Reader reader) throws MemoryOutOfBoundsException,
             MemoryOverFlowException, IOException, FileNotFoundIn, BracketsParseException;
+
+    /**
+     * Print the short syntax of the command which implement this interface
+     */
+    void rewrite();
+
+    /**
+     * @return String represent the syntax converted to the code color
+     */
+    String translate();
 }
 
