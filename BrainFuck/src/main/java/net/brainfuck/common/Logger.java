fde63f290c8aef28ddd082f26605bf7fc535a524
arsebac <mf403165@etu.unice.fr>
2016-11-05 11:03:51 +0100

    Logger crée et fonctionnel

--- BrainFuck/src/main/java/net/brainfuck/common/Logger.java
+++ BrainFuck/src/main/java/net/brainfuck/common/Logger.java
@@ -19,35 +19,56 @@ public class Logger {
 	private static int numMemoryMove  = 0;
 	private static long start = 0;
 
-	public static void countInstruction() {
-		numInstructions ++;
-	}
-
+	/**
+	 * Create a string with all informations collected.
+	 * @return informations
+	 */
 	public static String showResume() {
 		String retour ="PROG_SIZE : "+numInstructions+"\n";
 		retour+= "EXEC_TIME : "+(System.currentTimeMillis()-start)+" ms\n";
+		retour+= "EXEC_MOVE : "+numExecMove+"\n";
 		retour+= "DATA_MOVE : "+numMemoryMove+"\n";
 		retour+= "DATA_WRITE : "+numMemoryWrite+"\n";
 		retour+= "DATA_READ : "+numMemoryRead+"\n";
 		return retour;
 	}
 
+	/**
+	 * Count the number of instruction read.
+	 */
+	public static void countInstruction() {
+		numInstructions ++;
+	}
+	/**
+	 * Save the current timestamp of the start of the program
+	 * Useful to calculate the exec time with a subtraction.
+	 */
 	public static void startExecTime() {
 		start = System.currentTimeMillis();
 	}
 
+	/**
+	 * Count the number of times that data is stored in memory.
+	 */
 	public static void countMemoryWrite() {
 		numMemoryWrite++;
 	}
-
+	/**
+	 * Count the number of times that data is read in memory.
+	 */
 	public static void countMemoryRead() {
 		numMemoryRead++;
 	}
-
+	/**
+	 * Count the number of times that the pointer move in memory.
+	 */
 	public static void countMemoryMove() {
 		numMemoryMove++;
 	}
 
+	/**
+	 * Count the char read in the input file
+	 */
 	public static void countMove() {
 		numExecMove++;
 	}
