662456c24d3dd409365cebf36ab21e26b0a5ae1e
Thyvador <alex.hiltcher@gmail.com>
2016-11-06 17:11:29 +0100

    change space indent to tabs indent

--- BrainFuck/src/main/java/net/brainfuck/common/Logger.java
+++ BrainFuck/src/main/java/net/brainfuck/common/Logger.java
@@ -18,27 +18,28 @@ import java.io.FileWriter;
  *         on 05/11/2016
  */
 public class Logger {
-    private static int numInstructions = 0;
-    private static int numExecMove = 0;
-    private static int numMemoryWrite = 0;
-    private static int numMemoryRead = 0;
-    private static int numMemoryMove = 0;
-    private static long start = 0;
+	private static int numInstructions = 0;
+	private static int numExecMove = 0;
+	private static int numMemoryWrite = 0;
+	private static int numMemoryRead = 0;
+	private static int numMemoryMove = 0;
+	private static long start = 0;
 
-    private static BufferedWriter writer = null;
-    private static int step = 0;
+	private static BufferedWriter writer = null;
+	private static int step = 0;
 
 	/**
 	 * Create a string with all informations collected.
+	 *
 	 * @return informations
 	 */
 	public static String showResume() {
-		String retour ="PROG_SIZE : "+numInstructions+"\n";
-		retour+= "EXEC_TIME : "+(System.currentTimeMillis()-start)+" ms\n";
-		retour+= "EXEC_MOVE : "+numExecMove+"\n";
-		retour+= "DATA_MOVE : "+numMemoryMove+"\n";
-		retour+= "DATA_WRITE : "+numMemoryWrite+"\n";
-		retour+= "DATA_READ : "+numMemoryRead+"\n";
+		String retour = "PROG_SIZE : " + numInstructions + "\n";
+		retour += "EXEC_TIME : " + (System.currentTimeMillis() - start) + " ms\n";
+		retour += "EXEC_MOVE : " + numExecMove + "\n";
+		retour += "DATA_MOVE : " + numMemoryMove + "\n";
+		retour += "DATA_WRITE : " + numMemoryWrite + "\n";
+		retour += "DATA_READ : " + numMemoryRead + "\n";
 		return retour;
 	}
 
@@ -46,8 +47,9 @@ public class Logger {
 	 * Count the number of instruction read.
 	 */
 	public static void countInstruction() {
-		numInstructions ++;
+		numInstructions++;
 	}
+
 	/**
 	 * Save the current timestamp of the start of the program
 	 * Useful to calculate the exec time with a subtraction.
@@ -62,12 +64,14 @@ public class Logger {
 	public static void countMemoryWrite() {
 		numMemoryWrite++;
 	}
+
 	/**
 	 * Count the number of times that data is read in memory.
 	 */
 	public static void countMemoryRead() {
 		numMemoryRead++;
 	}
+
 	/**
 	 * Count the number of times that the pointer move in memory.
 	 */
@@ -76,72 +80,72 @@ public class Logger {
 	}
 
 
-    /**
-     * Increment by one the number of execution step.
-     */
-    public static void incrStep() {
-        Logger.step++;
-    }
+	/**
+	 * Increment by one the number of execution step.
+	 */
+	public static void incrStep() {
+		Logger.step++;
+	}
 
-    /**
-     * Set the writer to write in the log file.
-     *
-     * @param fileName the programme path and name.
-     * @throws IOException {@link IOException} if the file cannot be access.
-     */
-    public static void setWriter(String fileName) throws IOException {
-        if (!isWriterOpen()) {
-            try {
-                fileName = fileName.subSequence(0, fileName.lastIndexOf('.')) + ".log";
-                Logger.writer = new BufferedWriter(new FileWriter(fileName));
-            } catch (java.io.IOException e) {
-                throw new IOException("Impossible to access : " + fileName + ".log");
-            }
-        }
-    }
+	/**
+	 * Set the writer to write in the log file.
+	 *
+	 * @param fileName the programme path and name.
+	 * @throws IOException {@link IOException} if the file cannot be access.
+	 */
+	public static void setWriter(String fileName) throws IOException {
+		try {
+			fileName = fileName.subSequence(0, fileName.lastIndexOf('.')) + ".log";
+			Logger.writer = new BufferedWriter(new FileWriter(fileName));
+		} catch (java.io.IOException e) {
+			throw new IOException("Impossible to access : " + fileName + ".log");
+		}
+	}
 
-    /**
-     * Write an instruction execution log into the log file.
-     *
-     * @param executionPointer the index of the execution pointer.
-     * @param memory the memory.
-     * @throws IOException {@link IOException} if the file cannot be access.
-     */
-    public static void write(int executionPointer, Memory memory) throws IOException {
-        try {
-            step += 1;
-            writer.write(
-                    "============\n"+
-                    "Execution Step " + step +
-                    " : \nExecution pointer : " + executionPointer +
-                    ". Data pointer  : C" + memory.getIndex() +
-                    ".\nMemory : \n" + memory.toString() + "\n");
-            writer.flush();
-        } catch (java.io.IOException e) {
-            throw new IOException("Impossible to write in the file.");
-        }
-    }
+	/**
+	 * Write an instruction execution log into the log file.
+	 *
+	 * @param executionPointer the index of the execution pointer.
+	 * @param memory           the memory.
+	 * @throws IOException {@link IOException} if the file cannot be access.
+	 */
+	public static void write(int executionPointer, Memory memory) throws IOException {
+		try {
+			step += 1;
+			writer.write(
+					"============\n" +
+							"Execution Step " + step +
+							" : \nExecution pointer : " + executionPointer +
+							". Data pointer  : C" + memory.getIndex() +
+							".\nMemory : \n" + memory.toString() + "\n");
+			writer.flush();
+		} catch (java.io.IOException e) {
+			throw new IOException("Impossible to write in the file.");
+		}
+	}
 
-    /**
-     * Check if the writer was created.
-     * @return true if the writer is set, false otherwise.
-     */
-    public static boolean isWriterOpen() {
-        return writer != null;
-    }
+	/**
+	 * Check if the writer was created.
+	 *
+	 * @return true if the writer is set, false otherwise.
+	 */
+	public static boolean isWriterOpen() {
+		return writer != null;
+	}
 
-    /**
-     * Close the wrioter.
-     *
-     * @throws IOException {@link IOException} if the file cannot be access.
-     */
-    public static void closeWriter() throws IOException {
-        try {
-            writer.close();
-        } catch (java.io.IOException e) {
-            throw new IOException();
-        }
-    }
+	/**
+	 * Close the wrioter.
+	 *
+	 * @throws IOException {@link IOException} if the file cannot be access.
+	 */
+	public static void closeWriter() throws IOException {
+		try {
+			writer.close();
+		} catch (java.io.IOException e) {
+			throw new IOException();
+		}
+	}
+
 	/**
 	 * Count the char read in the input file
 	 */
--- BrainFuck/src/main/java/net/brainfuck/interpreter/Interpreter.java
+++ BrainFuck/src/main/java/net/brainfuck/interpreter/Interpreter.java
@@ -23,105 +23,105 @@ import static net.brainfuck.common.ArgumentConstante.*;
 
 public class Interpreter {
 	private Map<String, Language> interpretorExecuter = new HashMap<>();
-    private Executer executer;
+	private Executer executer;
 	private Reader reader;
-    private ArgumentAnalyzer argumentAnalyzer;
+	private ArgumentAnalyzer argumentAnalyzer;
 
-    /**
-     * Constructor which initialize attribute.
-     * @param reader Reader
-     * @param arg ArgumentAnalyzer use to get arguments
-     * @throws FileNotFoundException throw by setIo()
-     * @throws IOException throw bt BfImageWriter
-     */
-    public Interpreter(Reader reader, ArgumentAnalyzer arg, Executer executer) throws FileNotFoundException, IOException {
-        this.reader = reader;
-        this.argumentAnalyzer = arg;
-        this.executer = executer;
-        setIO();
-        if(arg.getFlags().contains(Context.TRANSLATE.getSyntax())) {
-	        executer.setImageWriter(new BfImageWriter());
-        }
-        if (arg.getFlags().contains(Context.TRACE.getSyntax())){
-            Logger.setWriter(arg.getArgument(PATH));
-        }
-    }
+	/**
+	 * Constructor which initialize attribute.
+	 * @param reader Reader
+	 * @param arg ArgumentAnalyzer use to get arguments
+	 * @throws FileNotFoundException throw by setIo()
+	 * @throws IOException throw bt BfImageWriter
+	 */
+	public Interpreter(Reader reader, ArgumentAnalyzer arg, Executer executer) throws FileNotFoundException, IOException {
+		this.reader = reader;
+		this.argumentAnalyzer = arg;
+		this.executer = executer;
+		setIO();
+		if(arg.getFlags().contains(Context.TRANSLATE.getSyntax())) {
+			executer.setImageWriter(new BfImageWriter());
+		}
+		if (arg.getFlags().contains(Context.TRACE.getSyntax())){
+			Logger.setWriter(arg.getArgument(PATH));
+		}
+	}
 
 	/**
 	 * Set default Input and output files depending of args "-i" and "-o"
 	 *
 	 * @throws FileNotFoundException if the path entered isn't valide, the file is missing and can't be open
 	 */
-    private void setIO() throws FileNotFoundException{
-        this.setIn();
-        this.setOut();
-    }
+	private void setIO() throws FileNotFoundException{
+		this.setIn();
+		this.setOut();
+	}
 
-    /**
-     * Set the default input to a files depending of args "-i"
-     *
-     * @throws FileNotFoundException throw by System.setIn()
-     */
-    private void setIn() throws FileNotFoundException {
-        String inPath = argumentAnalyzer.getArgument(IN_PATH);
-        if(inPath != null){
-            try {
-                System.setIn(new FileInputStream(inPath));
-            } catch (java.io.FileNotFoundException e) {
-                throw new FileNotFoundException(inPath);
-            }
-        }
-    }
+	/**
+	 * Set the default input to a files depending of args "-i"
+	 *
+	 * @throws FileNotFoundException throw by System.setIn()
+	 */
+	private void setIn() throws FileNotFoundException {
+		String inPath = argumentAnalyzer.getArgument(IN_PATH);
+		if(inPath != null){
+			try {
+				System.setIn(new FileInputStream(inPath));
+			} catch (java.io.FileNotFoundException e) {
+				throw new FileNotFoundException(inPath);
+			}
+		}
+	}
 
-    /**
-     * Set the default output to a files depending of args "-i"
-     *
-     * @throws FileNotFoundException throw by System.setOut()
-     */
-    private void setOut() throws FileNotFoundException {
-        String outPath = argumentAnalyzer.getArgument(OUT_PATH);
-        if(outPath != null){
-            try {
-                PrintStream printStream = new PrintStream(outPath);
-                System.setOut(printStream);
-            } catch (java.io.FileNotFoundException e) {
-                throw new FileNotFoundException(outPath);
-            }
-        }
-    }
+	/**
+	 * Set the default output to a files depending of args "-i"
+	 *
+	 * @throws FileNotFoundException throw by System.setOut()
+	 */
+	private void setOut() throws FileNotFoundException {
+		String outPath = argumentAnalyzer.getArgument(OUT_PATH);
+		if(outPath != null){
+			try {
+				PrintStream printStream = new PrintStream(outPath);
+				System.setOut(printStream);
+			} catch (java.io.FileNotFoundException e) {
+				throw new FileNotFoundException(outPath);
+			}
+		}
+	}
 
-    /**
-     * Interpret all characters which can be read with the attribute reader.
-     *
-     * @throws SyntaxErrorException {@link SyntaxErrorException} if an error of syntax is found.
-     * @throws MemoryOutOfBoundsException {@link MemoryOutOfBoundsException} if memory throw an exception.
-     * @throws IOException {@link IOException}  if reader throw an exception.
-     * @throws MemoryOverFlowException throw by memory
-     * @throws BracketsParseException throw by executer
-     */
-    public void interprate() throws IOException, SyntaxErrorException, MemoryOutOfBoundsException,
-            MemoryOverFlowException, FileNotFoundIn, BracketsParseException, FileNotFoundException {
-        String instruction;
-        Language currentInstruction;
+	/**
+	 * Interpret all characters which can be read with the attribute reader.
+	 *
+	 * @throws SyntaxErrorException {@link SyntaxErrorException} if an error of syntax is found.
+	 * @throws MemoryOutOfBoundsException {@link MemoryOutOfBoundsException} if memory throw an exception.
+	 * @throws IOException {@link IOException}  if reader throw an exception.
+	 * @throws MemoryOverFlowException throw by memory
+	 * @throws BracketsParseException throw by executer
+	 */
+	public void interprate() throws IOException, SyntaxErrorException, MemoryOutOfBoundsException,
+			MemoryOverFlowException, FileNotFoundIn, BracketsParseException, FileNotFoundException {
+		String instruction;
+		Language currentInstruction;
 
-        while ((instruction = reader.getNext()) != null) {
-            if ((currentInstruction = Language.languageMap.get(instruction)) == null) {
-                throw new SyntaxErrorException(instruction);
-            }
-            executer.execute(currentInstruction.getInterpreter());
-            Logger.countInstruction();
-        }
-        executer.end();
-    }
+		while ((instruction = reader.getNext()) != null) {
+			if ((currentInstruction = Language.languageMap.get(instruction)) == null) {
+				throw new SyntaxErrorException(instruction);
+			}
+			executer.execute(currentInstruction.getInterpreter());
+			Logger.countInstruction();
+		}
+		executer.end();
+	}
 
-    
-    void markReader() throws IOException {
-    	reader.mark();
-    }
-    
-    void resetReader() throws IOException, BracketsParseException {
-    	reader.reset();
-    }
-    
+
+	void markReader() throws IOException {
+		reader.mark();
+	}
+
+	void resetReader() throws IOException, BracketsParseException {
+		reader.reset();
+	}
+
 }
 
