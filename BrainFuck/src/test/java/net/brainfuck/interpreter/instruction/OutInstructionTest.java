46467bca725ecc5e6c2e758db948af46e63486a2
Francois Melkonian <francois.melkonian@etu.unice.fr>
2016-12-01 11:52:08 +0100

    Test unitaires non fait

--- BrainFuck/src/test/java/net/brainfuck/interpreter/instruction/OutInstructionTest.java
+++ BrainFuck/src/test/java/net/brainfuck/interpreter/instruction/OutInstructionTest.java
@@ -12,6 +12,7 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,35 +22,40 @@ import static org.junit.Assert.assertEquals;
  * @author Alexandre Hiltcher
  */
 public class OutInstructionTest {
-	private OutputStreamWriter outputStream = new OutputStreamWriter(System.out);
 	private Memory memory;
 	private OutInstruction instruction;
-	private String filename;
+	static List<Integer>out = new ArrayList();
+	private ExecutionReader reader;
 
+	// TODO : test des Out
 	/**
 	 * Sets the up.
 	 *
-	 */
 	@Before
 	public void setUp() throws Exception {
 
 		List<Language> langage = Arrays.asList(Language.LEFT,Language.LEFT);
-
-		instruction = new OutInstruction(outputStream);
-		ExecutionReader reader = new ExecutionReader(langage);
+		instruction = new OutInstruction(new OutputStreamWriter());
+		memory = new Memory();
+		reader = new ExecutionReader(langage);
 	}
+	 */
 
 	/**
 	 * Out.
 	 *
+	 */
 
 	 @Test
 	public void out() throws Exception {
 		memory.set('a');
-		instruction.execute(memory,reader);
-		assertEquals("a", outputStream.toString());
+		 instruction.execute(memory,reader);
+		 instruction.execute(memory,reader);
+		 instruction.execute(memory,reader);
+		 instruction.execute(memory,reader);
+		 System.out.println(out);
+		//assertEquals("a",out.get(0));
 	}
-	 */
 
 
 	/**
