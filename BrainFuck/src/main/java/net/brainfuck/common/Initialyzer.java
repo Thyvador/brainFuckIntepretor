9c2e38f478c443ace8c74a32f9f3150e55e22470
thyvador <alex.hiltcher@gmail.com>
2016-12-22 15:29:35 +0100

    Merge

--- BrainFuck/src/main/java/net/brainfuck/common/Initialyzer.java
+++ BrainFuck/src/main/java/net/brainfuck/common/Initialyzer.java
@@ -1,6 +1,7 @@
 package net.brainfuck.common;
 
 import net.brainfuck.Main;
+import net.brainfuck.common.executables.ExecutionReader;
 import net.brainfuck.exception.*;
 import net.brainfuck.exception.FileNotFoundException;
 import net.brainfuck.exception.IOException;
