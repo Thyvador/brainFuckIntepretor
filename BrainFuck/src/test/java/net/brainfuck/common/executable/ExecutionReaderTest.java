6ae1e049275702153c4a37a9ab7b4529ec1dd670
Jeremy Junac <bombers_06@hotmail.fr>
2016-12-30 12:00:04 +0100

    Tests calsses for function, procedure, ...

---
+++ BrainFuck/src/test/java/net/brainfuck/common/executable/ExecutionReaderTest.java
@@ -0,0 +1,19 @@
+package net.brainfuck.common.executable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExecutionReaderTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGenerate() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ BrainFuck/src/test/java/net/brainfuck/common/executable/ProcedureFunctionExecuteTest.java
@@ -0,0 +1,39 @@
+package net.brainfuck.common.executable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProcedureFunctionExecuteTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testRewrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTranslate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenerate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTrace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+}
