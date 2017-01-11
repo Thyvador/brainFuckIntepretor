7e3117cb04aab328e9c5139fcf4affcd02e8036d
David LANG <david.lang@etu.unice.fr>
2017-01-11 22:51:51 +0100

    ajout de commentaires

--- BrainFuck/src/main/java/net/brainfuck/interpreter/processing/MacroInterpreter.java
+++ BrainFuck/src/main/java/net/brainfuck/interpreter/processing/MacroInterpreter.java
@@ -22,6 +22,12 @@ class MacroInterpreter {
     }
 
 
+    /**
+     * Parse the name of a macro
+     *
+     * @param definition the definition to evaluate
+     * @return the string name of the macro
+     */
     private String getMacroName(String definition) {
         if (StringParser.containsParenthesis(definition))
             return definition.substring(0, definition.indexOf("("));
@@ -30,11 +36,24 @@ class MacroInterpreter {
         return definition;
     }
 
+    /**
+     * Return the argument of a macro
+     *
+     * @param instruction macro
+     * @return array of argument of macro
+     */
     private String[] getArgumentText(String instruction) {
         return StringParser.containsParenthesis(instruction) ?
                 StringParser.getArguments(instruction) : null;
     }
 
+    /**
+     * Convert array of String in an list of Integer
+     *
+     * @param arguments the arguments to convert
+     * @return List of Integer corresponding to the argument
+     * @throws SyntaxErrorException if an argument is not an Integer
+     */
     private List<Integer> getArgumentValue(String[] arguments) throws SyntaxErrorException {
         List<Integer> values = new ArrayList<>();
 
@@ -50,6 +69,13 @@ class MacroInterpreter {
         return values;
     }
 
+    /**
+     * Return the multiplicator of macro. For example : macro 2 => return 2
+     *
+     * @param str the string to evaluate
+     * @return the multiplicator of macro. For example : macro 2 => return 2
+     * @throws SyntaxErrorException if the multiplicator is not an Integer
+     */
     private int getMultiplicator(String str) throws SyntaxErrorException {
 
         // Récupération de ce qui se trouve derrière la macro (ex: macro 2 => 2  ou macro(2,3) 3 => 3)
@@ -76,10 +102,9 @@ class MacroInterpreter {
      *
      * @param instruction the instruction
      * @return true, if successful
-     * @throws IOException Signals that an I/O exception has occurred.
      * @throws BracketsParseException the brackets parse exception
      */
-    List<Language> writeMacro(String instruction) throws IOException, BracketsParseException, SyntaxErrorException {
+    List<Language> writeMacro(String instruction) throws  BracketsParseException, SyntaxErrorException {
         // Verification de parenthese : throw une erreur si c'est mal parenthese
         this.checkParentheses(instruction);
 
@@ -97,6 +122,11 @@ class MacroInterpreter {
         return programme;
     }
 
+    /**
+     * Check if the macro is well parenthesis
+     * @param instruction the macro to evaluate
+     * @throws SyntaxErrorException if the macro is not well parenthesis
+     */
     private void checkParentheses(String instruction) throws SyntaxErrorException {
         if (!StringParser.isCorrectParentheses(instruction)) {
             throw new SyntaxErrorException("Bad parenthesis " + instruction);
